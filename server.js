d7561e07d93b17f5ba7e7d2f56c54f975c6057ee
Cole Sammons <colemsammons@gmail.com>
2023-03-07 13:42:40 -0700

    mongodb set up

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+//uses dotenv throughout package
+require("dotenv").config();
 // express is a node.js framework
-const express = require('express');
+const express = require("express");
 const app = express();
 // provides utilities for working with file and directory paths
-const path = require('path');
-const errorHandler = require('./middleware/errorHandler');
-const { logger } = require('./middleware/logger');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
+const path = require("path");
+const errorHandler = require("./middleware/errorHandler");
+const { logger } = require("./middleware/logger");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logEvents } = require("./middleware/logger");
 const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
+
+// Uses custom connection instead of default
+connectDB();
 
 app.use(logger);
 
@@ -22,22 +32,33 @@ app.use(express.json());
 app.use(cookieParser());
 
 // express.static acts as middleware, telling the server where to find static files
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, "public")));
 
-app.use('/', require('./routes/root'));
+app.use("/", require("./routes/root"));
 
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, './views/404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' });
-    } else {
-        res.type('txt').send('404 Not Found');
-    }
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "./views/404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 // Using custom error handler that overrides express default
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
