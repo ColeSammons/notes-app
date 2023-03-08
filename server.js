b08209f9338286808ce603bb37860d6647d4e162
Cole Sammons <colemsammons@gmail.com>
2023-03-08 14:39:12 -0700

    started setting up controllers

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
 
+// controller for /user endpoint
+app.use('/users', require('./routes/userRoutes'));
+
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
